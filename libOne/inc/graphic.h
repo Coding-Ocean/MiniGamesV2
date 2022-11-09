df64069ce2b173a32b27fd4b330a046a27f6959e
Coding-Ocean <eijo524@gmail.com>
2022-11-09 17:16:16 +0900

    new point, image

--- libOne/inc/graphic.h
+++ libOne/inc/graphic.h
@@ -42,6 +42,17 @@ void arc(float x, float y, float startAngle, float angle, float radius);
 void rect(float x, float y, float w, float h);
 void rect(float x, float y, float w, float h, float angle);
 void circle(float x, float y, float diameter);
+void point(const class VECTOR& p,
+	const struct COLOR& strokeColor, float strokeW);
+void line(const class VECTOR& p1, const class VECTOR& p2,
+	const struct COLOR& strokeColor, float strkWeight);
+void arrow(const class VECTOR& s, const class VECTOR& e,
+	const struct COLOR& strokeColor, float strokeW);
+void circle(const class VECTOR& p, float diameter,
+	const struct COLOR& fillColor, const struct COLOR& strokeColor, float strokeW);
+void rect(const class VECTOR& p, float w, float h, float angle,
+	RECT_MODE mode, 
+	const struct COLOR& fillColor, const struct COLOR& strokeColor, float strokeW);
 
 struct SHAPE_VERTEX { float x, y; };
 int createShape(struct SHAPE_VERTEX* vertices, int numVertices);
@@ -58,6 +69,7 @@ void imageColor(float r, float g, float b);
 void imageColor(float r, float g, float b, float a);
 void imageColor(float c);
 void image(int idx, float x, float y, float angle = 0, float size = 1);
+void image(int idx, const class VECTOR& p, float angle=0, float size=1, RECT_MODE mode=CORNER);
 
 void triangle3D(const class VECTOR& p0, const class VECTOR& p1, const class VECTOR& p2,
             const COLOR& c0=COLOR(), const COLOR& c1=COLOR(), const COLOR& c2=COLOR());
